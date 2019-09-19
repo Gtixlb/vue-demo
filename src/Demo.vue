42013f571d88b3c223e3f5bcd56b06f6ef3f4780
admin <1150901068@qq.com>
2019-09-19 13:13:14 +0800

    function:vue-demo测试

---
+++ src/Demo.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <input v-model="inputValue" />
+    <button @click="clickSubmit">提交</button>
+    <ul v-for="(item,index) in list":key="index">
+      <li>{{item}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+
+export default {
+  //sssss
+  //data : function () {
+  data () {
+     return {
+      inputValue: '',
+      list: []
+     }
+  },
+  methods: {
+    clickSubmit () {
+      this.list.push(this.inputValue)
+      this.inputValue = ''
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
